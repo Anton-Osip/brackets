2cded6ec6ef70da8f217999f897967f9f0bc0716
Anton-Osip <osipchika@gmail.com>
2024-08-09 15:59:45 +0300

    feat: task is complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let myBracketsConfig = bracketsConfig.map(item => item[0] + item[1])
+	let myStr = str
+	let count = str.length
+
+	do {
+		count--
+		myBracketsConfig.forEach(element => {
+			myStr=myStr.replace(element, '')
+		})
+	} while (count !== 0)
+	return myStr.length === 0
 }
